7225929fcb62d0fc41d0347c1d70accf2fd4b7ec
jhon-frand <frandlebaza@gmail.com>
2023-12-12 09:04:49 -0500

    commit init

---
+++ app/Models/Curso.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Curso extends Model
+{
+    use HasFactory;
+    public function profesor(){
+        return $this->belongsTo(Profesor::class);
+    }
+    public function matriculas(){
+        return $this->hasMany(Matricula::class);
+    }
+}
---
+++ database/migrations/2023_12_01_135147_create_matriculas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matriculas', function (Blueprint $table) {
+            $table->id();
+            $table->string('estudiante');
+            $table->string('curso');
+            $table->string('fecha_matricula');
+            $table->string('calificacion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matriculas');
+    }
+};
